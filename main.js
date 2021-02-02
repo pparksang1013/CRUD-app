37c64580ff12afb7d889fcf59377040df820fb26
pparksang1013 <65754137+pparksang1013@users.noreply.github.com>
2021-02-02 21:32:49 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let crudApp = new function(){
+let crudApp = new function() {
     this.myClass = [
         {ID: 1, className: '운영체제', Category: '전공필수', Credit: 3},
         {ID: 2, className: '컴퓨터구조론', Category: '전공선택', Credit: 4},
@@ -16,14 +16,35 @@ let crudApp = new function(){
     
     //col에 새로운 table header에 해당하는 데이터 ( myClass의  key 값) 들을 넣어주는 코드
     //비어있는 col 배열에 myClass 배열 속 객체들의 key값들을 넣어줘야 한다.
-    for(let i = 0, i < this.myClass.length; i++){
+    for(let i = 0; i < this.myClass.length; i++ ) {
         //각 객체들 속의 key값들 순화
-        for(let key in this.myClass[i]);
+        for(let key in this.myClass[i]) {
             //key를 col배열에 담기
-            if(this.col.indexOf(key) === -1) this.col.push)(key)};
+            if(this.col.indexOf(key) === -1) this.col.push(key);
+
+                    };
+ 
+                };
+
+                let table = document.createElement('table');
+                table.setAttribute('id','classTable');
+                //tr table row 새로운 행 추가.
+                let tr = table.insertRow(-1);
+
+                //th table header
+                for(let h = 0; h < this.col.length; h++){
+                    let th = document.createElement('th');
+                    th.innerHTML = this.col[h];
+                    tr.appendChild(th);
+                };
+
+                let div = document.getElementById('container');
+                div.innerHTML = '수강관리 앱';
+                div.appendChild(table);
+
+            };     
+
         };
-    };
-};
 
 crudApp.createTable();
 
