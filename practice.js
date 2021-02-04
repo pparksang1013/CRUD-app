d5c7fc4d4ec6505db979bbda065b37d36a9b3935
pparksang1013 <65754137+pparksang1013@users.noreply.github.com>
2021-02-04 22:52:39 +0900

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,15 @@
+function addRow() {
+    // table element 찾기
+    const table = document.getElementById('fruits');
+    
+    // 새 행(Row) 추가 (테이블 중간에)
+    const newRow = table.insertRow(2);
+    
+    // 새 행(Row)에 Cell 추가
+    const newCell1 = newRow.insertCell(0);
+    const newCell2 = newRow.insertCell(1);
+    
+    // Cell에 텍스트 추가
+    newCell1.innerText = '새 과일';
+    newCell2.innerText = 'New Fruit';
+  }
